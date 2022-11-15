8ea033a6e4e308ffc88807ea389eb2860c940731
110b13123 <115352680+110b13123@users.noreply.github.com>
2022-11-15 23:36:34 +0800

    Create Class08.java

---
+++ Class08.java
@@ -0,0 +1,12 @@
+import pack6.subpack1.subsubpack.CStyle;
+public class hw12_8
+{
+   public static void main(String args[])
+   {
+      CCircle cir=new CCircle();
+      CRectangle rect=new CRectangle();
+      CStyle sty=new CStyle(1);
+      cir.show();
+      rect.show();
+   }
+}
