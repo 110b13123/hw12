5ee35e44af6f37a171330ea16e96d463b6ae76c5
110b13123 <115352680+110b13123@users.noreply.github.com>
2022-11-15 23:37:30 +0800

    Create Class9.java

---
+++ Class9.java
@@ -0,0 +1,10 @@
+public class hw12_9
+{
+   public static void main(String args[])  
+   {
+      CSphere sp=new CSphere(2);
+      CTrapezoid tra=new CTrapezoid(2,3,4);
+      sp.show();
+      tra.show();
+   }
+}
